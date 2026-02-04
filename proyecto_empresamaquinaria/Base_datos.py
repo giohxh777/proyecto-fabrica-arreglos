11ee26432d9c26570d6984adc81a6ea7d671cb35
Giovanny Rueda <giovannirueda588@gmail.com>
2026-02-04 08:48:32 -0500

    Update Base_datos.py

--- proyecto_empresamaquinaria/Base_datos.py
+++ proyecto_empresamaquinaria/Base_datos.py
@@ -13,4 +13,46 @@ class Api_BD:
         self.Api_datos.extend(lista_empleados)
 
 
+empleado_1 = "Empleado 1: jonnatan Rueda 1093591113 321-7297443"
+empleado_2 = "Empleado 2: Carlos Perez 52370510 321-7297444"
+empleado_3 = "Empleado 3: Ana Gomez 13717353 321-7297445"
+
+empleados = [empleado_1, empleado_2, empleado_3]
+empleados_adicionales = [empleado_2, empleado_3]
+
+api = Api_BD()
+
+for empleado in empleados:
+    api.guardar_empleado(empleado)
+
+print("Lista inicial:")
+api.imprimir_Api()
+
+api.guardar_empleado(empleado_1)
+api.imprimir_Api()
+
+api.extender_Api(empleados_adicionales)
+api.imprimir_Api()
+
+api.Api_datos.insert(0, empleado_3)
+api.imprimir_Api()
+
+api.Api_datos.pop(1)
+api.imprimir_Api()
+
+api.Api_datos.remove(empleado_2)
+api.imprimir_Api()
+
+print(f"Índice de empleado 3: {api.Api_datos.index(empleado_3)}")
+print(f"Cantidad de empleado 3: {api.Api_datos.count(empleado_3)}")
+
+api.Api_datos.sort()
+api.imprimir_Api()
+
+api.Api_datos.reverse()
+api.imprimir_Api()
+
+
+
+
 
