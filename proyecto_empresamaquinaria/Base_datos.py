9477525ac0ef375802777d9e5ff123990b54c09c
Giovanny Rueda <giovannirueda588@gmail.com>
2026-02-05 16:56:05 -0500

    Update Base_datos.py

--- proyecto_empresamaquinaria/Base_datos.py
+++ proyecto_empresamaquinaria/Base_datos.py
@@ -7,50 +7,55 @@ class Api_BD:
         
     def imprimir_Api(self):
         for empleado in self.Api_datos:
-            print(empleado)
+            print(empleado.ver_info())
             
     def extender_Api(self, lista_empleados):
         self.Api_datos.extend(lista_empleados)
-
-
-empleado_1 = "Empleado 1: jonnatan Rueda 1093591113 321-7297443"
-empleado_2 = "Empleado 2: Carlos Perez 52370510 321-9214833"
-empleado_3 = "Empleado 3: Ana Gomez 13717353 322-7658907"
-
-empleados = [empleado_1, empleado_2, empleado_3]
-empleados_adicionales = [empleado_2, empleado_3]
-
-api = Api_BD()
-
-for empleado in empleados:
-    api.guardar_empleado(empleado)
-
-print("Lista inicial:")
-api.imprimir_Api()
-
-api.guardar_empleado(empleado_1)
-api.imprimir_Api()
-
-api.extender_Api(empleados_adicionales)
-api.imprimir_Api()
-
-api.Api_datos.insert(0, empleado_3)
-api.imprimir_Api()
-
-api.Api_datos.pop(1)
-api.imprimir_Api()
-
-api.Api_datos.remove(empleado_2)
-api.imprimir_Api()
-
-print(f"Índice de empleado 3: {api.Api_datos.index(empleado_3)}")
-print(f"Cantidad de empleado 3: {api.Api_datos.count(empleado_3)}")
-
-api.Api_datos.sort()
-api.imprimir_Api()
-
-api.Api_datos.reverse()
-api.imprimir_Api()
+        
+    def insertar_empleado(self, posicion, obj_nuevo_empleado):
+        self.Api_datos.insert(posicion, obj_nuevo_empleado)
+        
+    def eliminar_empleado_por_indice(self, indice):
+        self.Api_datos.pop(indice)
+        
+    def remover_empleado_por_cedula(self, cedula):
+        for empleado in self.Api_datos:
+            if empleado.get_cedula_empleado() == cedula:
+                self.Api_datos.remove(empleado)
+        
+    def obtener_indice_empleado(self, cedula):
+        for i, empleado in enumerate(self.Api_datos):
+            if empleado.get_cedula_empleado() == cedula:
+                return i
+        return -1
+        
+    def contar_empleados(self):
+        return len(self.Api_datos)
+        
+    def ordenar_empleados_por_nombre(self):
+        nombres = []
+        for empleado in self.Api_datos:
+            nombres.append(empleado.get_nombre_empleado())
+        nombres.sort()
+        empleados_ordenados = []
+        for nombre in nombres:
+            for empleado in self.Api_datos:
+                if empleado.get_nombre_empleado() == nombre:
+                    empleados_ordenados.append(empleado)
+                    break
+        self.Api_datos = empleados_ordenados
+        
+    def invertir_orden_empleados(self):
+        self.Api_datos.reverse()
+        
+    def buscar_empleado_por_cedula(self, cedula):
+        for empleado in self.Api_datos:
+            if empleado.get_cedula_empleado() == cedula:
+                return empleado
+        return None
+        
+    def obtener_todos_empleados(self):
+        return self.Api_datos
 
 
 
